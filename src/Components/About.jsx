96fbe852da959136622ab439bfc7320f1a60d68d
Abhinav Sharma <89687523+Abhinav5050649@users.noreply.github.com>
2023-09-22 13:33:12 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,17 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Student pursuing a B.Tech in Computer Engineering at Pandit Deendayal Energy University(PDEU), Gandhinagar, India";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Full-Stack Development",
+  "Cloud-Based Development",
+  "Team Management",
+  "Project Management",
 ];
 
 /**
@@ -45,7 +43,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about applying my creative mind to find practical solutions to real-life.";
 
 const About = () => {
   return (
